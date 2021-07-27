94083f70b79aec91fde10a2283fc9f02ef374448
Mr-luckY93 <alric9382@gmail.com>
2021-07-27 22:15:22 +0100

    clean

--- login.js
+++ login.js
@@ -1,17 +1,18 @@
-import { Selector } from "testcafe";
-
 fixture`Log in`.page("https://www.etalente.co.za/");
+
+const email = "alric9382@gmail.com";
+const password = "Mathilde07";
 
 test("Log in", async (t) => {
   await t
-    .typeText("#mat-input-0", "alric9382@gmail.com")
-    .typeText("#mat-input-1", "Mathilde07")
+    .typeText("#mat-input-0", email)
+    .typeText("#mat-input-1", password)
     .click(".mat-focus-indicator");
 });
 
 test("Fail Log in", async (t) => {
   await t
-    .typeText("username", "alric9382@gmail.com")
-    .typeText("#mat-input-1", "Mathilde07")
+    .typeText("username", email)
+    .typeText("#mat-input-1", password)
     .click(".mat-focus-indicator");
 });
